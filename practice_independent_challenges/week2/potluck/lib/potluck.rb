0bd90f8a866fddd20b4783bc323a5cd692322330
Renee Messersmith <morganrmessersmith@gmail.com>
2024-07-10 21:59:32 -0400

    Feat: finish week2 IC

--- practice_independent_challenges/week2/potluck/lib/potluck.rb
+++ practice_independent_challenges/week2/potluck/lib/potluck.rb
@@ -19,6 +19,23 @@ class Potluck
     end
 
     def menu
-        
+        event_menu = {}
+        dishes.each do |dish|
+            if dish.category == :appetizer
+                event_menu[:appetizer] = dish.name
+            elsif dish.category == :entre
+                event_menu[:entre] = dish.name
+            elsif dish.category == :dessert
+                event_menu[:dessert] = dish.name
+            end
+        end
+        return event_menu
+    end
+
+    def ratio(category)
+        dishes_by_category = dishes.find_all do |dish|
+            dish.category == category
+        end
+        return dishes_by_category.count.to_f / dishes.count.to_f * 100
     end
 end
